81efcaaafaeb3d207abd2be395d9c5d24b685921
Jaeseong Jeong <js104427@gmail.com>
2024-04-17 22:48:18 +0800

    feat: added pages (home, exercise) added components (navbar, footer, herobanner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { Box } from "@mui/material";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
-export default App
+export default App;
